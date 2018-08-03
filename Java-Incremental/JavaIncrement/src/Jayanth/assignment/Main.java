782eae1b71a42abc230a587be68fb8a4f684e3e0
Jayanth kumar Apoor <apoorjayanth1@gmail.com>
2018-08-03 12:31:02 +0530

    Updated

--- Java-Incremental/JavaIncrement/src/Jayanth/assignment/Main.java
+++ Java-Incremental/JavaIncrement/src/Jayanth/assignment/Main.java
@@ -2,12 +2,12 @@ package Jayanth.assignment;
 
 class Main{
     public static void main(String[] args){
-        int p,q;
+        
         Data objA = new Data();
         objA.printing();
-        //objA.localVariablesPrint(p,q); //It shows compile error because local variables are not initialised
+        //objA.localVariablesPrint(); //It shows compile error because local variables are not initialised
 
-        Singleton obj = Singleton.prints("Hi");
+        Singleton obj = Singleton.prints("Hi"); // It will creates singleton object and initializes with string
         obj.nonStatic();
 
 
