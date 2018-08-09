f2d63fabf32597e9d22f590bd6662afa05e8efb8
Jayanth kumar Apoor <apoorjayanth1@gmail.com>
2018-08-09 22:34:48 +0530

    changes done

--- Java-Incremental/Assignment-6/TestSlist.java
+++ Java-Incremental/Assignment-6/TestSlist.java
@@ -1,20 +1,19 @@
-
 import java.util.*;
 
 class SList<E> {
 
-	public Link<E> headLink = new Link<E>(null);
+	public  Link<E> headLink = new Link<E>(null);
 	SListIterator<E> iterator() { 
               return new SListIterator<E>(headLink); }       //creating instance of SlistIterator
 	public String toString() { 
-		if(headLink.next == null) return "SList: []";
-		System.out.println("SList: [");
+		if(headLink.next == null) return "List";
+		System.out.println("List: ");
 		SListIterator<E> it = this.iterator();
 		StringBuilder s = new StringBuilder(); 
 		while(it.hasNext()) {
 			s.append(it.next() + (it.hasNext() ? ", " : ""));
 		}
-		return s + "]";
+		return s + "";
 	}		
 }
 
@@ -30,14 +29,27 @@ class SListIterator<E> {
 		current = current.next;
 		return current;
 	}
+
 	public void insert(E e) {        // insert method
+		
+        while(current.next!=null)
+			current=current.next;
 		current.next = new Link<E>(e, current.next);
-		current = current.next;
-	}	
-	public void remove() {           // remove method
-		if(current.next != null) {
-			current.next = current.next.next;
-		}
+	
+	}
+
+	public void remove(E e) {           // remove method
+		
+        Link<E> prev = null;            
+        while(current.e != e){
+        	prev=current;               // storing previous link reference
+        	current=current.next;
+        	
+        }
+
+        Link<E> p = current.next;
+        prev.next = p;
+
 	}
 }
 
@@ -62,16 +74,15 @@ public class TestSlist {
 		SList<Integer> sl = new SList<Integer>();
 		System.out.println(sl);
 		SListIterator<Integer> sit = sl.iterator();
-                System.out.println("Inserting elements : ");
+        System.out.println("Inserting elements : ");
 		sit.insert(1);                   // inserting elements in linked list
-                sit.insert(2);
-                sit.insert(3);
+        sit.insert(2);
+        sit.insert(3);
 		System.out.println(sl);
-                System.out.println("Removing one element");
-                SListIterator<Integer> sit1 = sl.iterator();
-                sit1.remove();                   // removing element from linked list
-                System.out.println(sl);
+        System.out.println("Removing one element");
+        SListIterator<Integer> sit1 = sl.iterator();
+        sit1.remove(2);                   // removing element from linked list
+        System.out.println(sl);
 			
 	}
 }
-     
